4dbf365c033543215c44fdf4e884a7e0d5012d5c
FLS <1549367453@qq.com>
2023-03-21 10:22:38 +0800

    第二次提交项目！增加了private修饰的变量

--- src/Test01.java
+++ src/Test01.java
@@ -1,6 +1,40 @@
 
 public class Test01 {
+    private String name;
+    int age;
+
+    public Test01(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Test01{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
     public static void main(String[] args) {
         System.out.println("git第一个测试文件");
+        Test01 one = new Test01("FLS",22);
+        System.out.println(one);
     }
 }
